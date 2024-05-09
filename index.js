6fca5851bc363c6526f601c0c3ab7d75088db82b
matheusrenner22 <matheusrennerdev@outlook.com>
2024-05-09 17:07:08 -0300

    Fiz um fix adicionando os milissegundos e refatorando o código

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const relogio = () => {
     const relogio = document.querySelector('.relogio');
     const iniciar = document.querySelector('.iniciar');
+    const horaDom = document.querySelector('#hora');
+    const minutosDom = document.querySelector('#minutos');
+    const segundosDom = document.querySelector('#segundos');
+    const milissegundosDom = document.querySelector('#milissegundos');
+
     let running = false;
+    let hora = 0;
+    let minutos = 0;
     let segundos = 0;
-    let timer;
-
-
-    function criaSegundosDaHora(segundos) {
-        const data = new Date(segundos * 1000);
-
-        return data.toLocaleTimeString('pt-BR', {
-            hour12: false,
-            timeZone: 'UTC'
-        });
-    }
-
+    let milissegundos = 0;
+    let cronometro;
 
     function iniciaRelogio() {
-        clearInterval();
-        timer = setInterval(() => {
-            segundos++;
-            relogio.innerHTML = criaSegundosDaHora(segundos);
-        }, 100);
+        clearInterval(cronometro);
+        cronometro = setInterval(() => { timer(); }, 10);
         relogio.classList.remove('pausando');
         running = true;
         iniciar.innerHTML = "pausar"
     }
 
     function pausarRelogio() {
-        clearInterval(timer);
+        clearInterval(cronometro);
         running = false;
         iniciar.innerHTML = 'continuar'
         relogio.classList.remove('iniciando');
@@ -37,9 +31,41 @@ const relogio = () => {
     function zerarRelogio() {
         iniciar.innerHTML = 'iniciar';
         running = false;
-        clearInterval(timer);
-        relogio.innerHTML = '00:00:00';
+        hora = 0;
+        minutos = 0;
         segundos = 0;
+        milissegundos = 0;
+
+        horaDom.innerHTML = '00';
+        minutosDom.innerHTML = '00';
+        segundosDom.innerHTML = '00';
+        milissegundosDom.innerHTML = '000';
+    }
+
+    function timer() {
+        if ((milissegundos += 10) === 1000) {
+            milissegundos = 0;
+            segundos++;
+        }
+
+        if (segundos === 60) {
+            segundos = 0;
+            minutos++;
+        }
+
+        if (minutos === 60) {
+            minutos = 0;
+            hora++;
+        }
+
+        horaDom.innerHTML = retornaData(hora);
+        minutosDom.innerHTML = retornaData(minutos);
+        segundosDom.innerHTML = retornaData(segundos);
+        milissegundosDom.innerHTML = retornaData(milissegundos);
+    }
+
+    function retornaData(input) {
+        return input > 10 ? input : `0${input}`;
     }
 
 
